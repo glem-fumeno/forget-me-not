44d044d7ead73d0f8f275ce796917ddecf1020cd
milkoverflow <alicja.rrosiak@gmail.com>
2025-07-09 07:56:33 +0200

    enabled recipes endpoints

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from api.carts.endpoints import endpoints as carts
 from api.docs.endpoints import blueprint
 from api.items.endpoints import endpoints as items
 from api.logger import init_logger
+from api.recipes.endpoints import endpoints as recipes
 from api.users.endpoints import endpoints as users
 from config import get_config
 
@@ -24,6 +25,7 @@ app.register_blueprint(blueprint)
 app.register_blueprint(users.blueprint)
 app.register_blueprint(items.blueprint)
 app.register_blueprint(carts.blueprint)
+app.register_blueprint(recipes.blueprint)
 
 
 if __name__ == "__main__":
